405297fbdd706dc63bfcad5353f2144af23385aa
kajal <kajal.sund9@gmail.com>
2025-07-30 21:52:38 -0400

    test case update

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 def test_root():
-    response = client.get("/")
+    response = client.get("/home")
     assert response.status_code == 200
     assert response.json() == {"message": "Task Manager API running!"}
